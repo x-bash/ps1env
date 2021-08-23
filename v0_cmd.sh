6a7f7c664cdd0a034f6f01c2cb94552681ca2155
L <l@x-cmd.com>
2021-08-23 21:31:26 +0800

    ...

---
+++ v0_cmd.sh
@@ -0,0 +1,22 @@
+xrc param/v0
+
+. ./v0
+
+cmd(){
+    param <<A
+subcommand:
+    sub1        "s"
+    sub2        "s2"
+    sub3        "s3"
+A
+
+    echo "subcomand: $PARAM_SUBCMD"
+}
+
+cmd_cli(){
+    cli_init cmd1
+    cli_bind "+sub1" "cmd sub1"
+    cli_bind "+sub2" "cmd sub2"
+    cli_bind "+sub3" "cmd sub3"
+    cli_bind "+q" "cli_reset"
+}
---
+++ v0_cmd2.sh
@@ -0,0 +1,22 @@
+xrc param/v0
+
+. ./v0
+
+cmd2(){
+    param <<A
+subcommand:
+    csub1        "s"
+    csub2        "s2"
+    csub3        "s3"
+A
+
+    echo "subcomand: cmd2 $PARAM_SUBCMD"
+}
+
+cmd2_cli(){
+    cli_init cmd2
+    cli_bind "+csub1" "cmd2 csub1"
+    cli_bind "+csub2" "cmd2 csub2"
+    cli_bind "+csub3" "cmd2 csub3"
+    cli_bind "+q" "cli_reset"
+}
