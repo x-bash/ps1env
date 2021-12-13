4eb6dd8ab1dc333cb5435752b234a89e90ff979d
edwinjhlee <edwin.jh.lee@gmail.com>
2021-12-13 16:46:08 +0800

    ...

--- v0_cmd.sh
+++ v0_cmd.sh
@@ -5,18 +5,19 @@ xrc param/v0
 . ./v0
 
 cmd(){
-    param <<A
+    param:dsl <<A
 subcommand:
     sub1        "s"
     sub2        "s2"
     sub3        "s3"
 A
+    param:run
 
     echo "subcomand: $PARAM_SUBCMD"
 }
 
 cmd_ps1env(){
-    ps1env init "cmd1>"
+    ps1env init "cmd1"
     ps1env alias "+sub1" "cmd sub1"
     ps1env alias "+sub2" "cmd sub2"
     ps1env alias "+sub3" "cmd sub3"
